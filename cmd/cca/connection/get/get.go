cfebf066f957da245d1d5aeda460129b9b982ac6
Khosrow Moossavi <khos2ow@gmail.com>
2019-08-06 16:37:23 -0400

    Add 'connection get' command

--- cmd/cca/connection/get/get.go
+++ cmd/cca/connection/get/get.go
@@ -12,32 +12,41 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package connection implements the `connection` command
-package connection
+// Package get implements the `connection get` command
+package get
 
 import (
-	"github.com/cloud-ca/cca/cmd/cca/connection/get"
-	"github.com/cloud-ca/cca/cmd/cca/connection/list"
 	"github.com/cloud-ca/cca/pkg/cli"
-	"github.com/cloud-ca/cca/pkg/util"
+	"github.com/cloud-ca/cca/pkg/output"
 	"github.com/spf13/cobra"
 )
 
-// NewCommand returns a new cobra.Command for connection
+type flag struct {
+	id string
+}
+
+// NewCommand returns a new cobra.Command for connection get
 func NewCommand(cli *cli.Wrapper) *cobra.Command {
+	flg := &flag{}
 	cmd := &cobra.Command{
 		Args:  cobra.NoArgs,
-		Use:   "connection",
-		Short: "Manage service connections that you can create resources for",
-		Long: util.LongDescription(`
-            Service connections are the services that you can create resources for (e.g. compute, object
-            storage). Environments are created for a specific service which allows you to create and
-            manage resources within that service.
-        `),
+		Use:   "get",
+		Short: "Get service connection",
+		Long:  "Get service connection",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			connection, err := cli.CcaClient.ServiceConnections.Get(flg.id)
+			if err != nil {
+				return err
+			}
+			return cli.OutputBuilder.Build(func(formatter *output.Formatter) error {
+				return formatter.Format(connection)
+			})
+		},
 	}
 
-	cmd.AddCommand(get.NewCommand(cli))
-	cmd.AddCommand(list.NewCommand(cli))
+	cmd.Flags().StringVar(&flg.id, "id", "", "environment id")
+
+	cmd.MarkFlagRequired("id")
 
 	return cmd
 }
