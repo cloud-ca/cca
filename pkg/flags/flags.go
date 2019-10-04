5fb644dea8e343df03259625754437597572f8c3
Khosrow Moossavi <khos2ow@gmail.com>
2019-10-04 15:39:25 -0400

    Normaliazing godocs package comments (#20)

--- pkg/flags/flags.go
+++ pkg/flags/flags.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package flags contains general utility of the cca cli flags
 package flags
 
 import (
